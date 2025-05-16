ae0e77b3dbd9299d75097f239b797af5cc57fe2b
Edgar Yeghiazaryan <yeghiazaryanedgar93@gmail.com>
2025-05-16 13:44:20 +0400

    FeaturedSection.jsx style update

--- frontend/src/components/Products/FeaturedCollection.jsx
+++ frontend/src/components/Products/FeaturedCollection.jsx
@@ -6,7 +6,7 @@ function FeaturedCollection() {
     return (
         <section className="lg:px-0">
             <div className="lg:container mx-auto flex flex-col-reverse lg:flex-row bg-gray-50 lg:rounded-lg">
-                <div className="lg:w-1/2 px-14 py-12 sm:py-12 md:py-12 lg:py-20 text-center lg:text-left">
+                <div className="lg:w-1/2 px-14 py-12 sm:py-12 md:py-12 lg:py-34 text-center lg:text-left">
                     <h2 className="text-lg font-semibold text-gray-700 mb-3">Comfort and Style</h2>
                     <h2 className="text-2xl md:text-4xl font-bold mb-6">Apparel made for your everyday life</h2>
                     <div className="text-lg text-gray-600 mb-10">
